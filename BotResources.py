818c8a4f614e25b5996c1339c02dfa88a9ea22c8
AdminBenni <benediktaronsigurorsson@gmail.com>
2018-07-16 16:30:47 +0000

    Decisions, Bets and more

--- BotResources.py
+++ BotResources.py
@@ -1,6 +1,9 @@
 from random import randint
 from BTYPE import cNum
+import datetime
 
+
+'''     Class that handles Rock Paper Scissor games     '''
 class RPSGame:
 
     opts = ["rock", "scissors", "paper"]
@@ -32,17 +35,12 @@ class RPSGame:
     def comp_wins(self):
         return self.__comp_wins
 
-    def __str__(self):
-        return str(vars(self))
-
-    __repr__ = __str__
-
-    def status(self, rounds=False):
+    def status(self, rounds=False):     # Returns information about the game in a readable manner
         return "SNMBot wins: " + str(self.__comp_wins) +\
                "\n<@" + self.__userID + "> wins: " + str(self.__user_wins) +\
                (("\nRounds Completed: " + str(self.__round)) if rounds else "")
 
-    def play_round(self, word):
+    def play_round(self, word):     #Initiates a round of rock paper scissors where the bot chooses at random and returns the result
         if word.lower() in ("", " "):
             return "You have to chose at least one option <@" + self.__userID + ">!\nThe options are: " + str(self.opts) + ".\nTry again!"
         elif word.lower() not in self.opts:
@@ -66,14 +64,48 @@ class RPSGame:
             self.__round += 1
             return mes + "\n\nResults after round " + str(self.__round) + ":\n" + self.status()
 
+    def __str__(self):
+        return str(vars(self))
 
+    __repr__ = __str__
+
+
+
+'''     Class that handles betting      '''
+class Bet:
+    def __init__(self, userID, opts):
+        self.__userID = userID
+        self.__opts = opts
+
+    @property
+    def userID(self):
+        return self.__userID
+
+    @property
+    def opts(self):
+        return self.__opts
+
+    def __str__(self):
+        return str(vars(self))
+
+    __repr__ = __str__
+
+
+'''     Class that handles information about users that use the bot     '''
 class User:
-    def __init__(self, userID, rps_games=0, rps_wins=0, rps_losses=0, rps_game=None):
+    def __init__(self, userID, rps_games=0, rps_wins=0, rps_losses=0, rps_game=None, snm_points=0, last_snm_point_update=None, bet_games=0, bet_wins=0, bet_losses=0, bet_game=None, ongoing_bets={}):
         self.__userID = userID
         self.__rps_games = rps_games
         self.__rps_wins = rps_wins
         self.__rps_losses = rps_losses
         self.__rps_game = rps_game
+        self.__snm_points = snm_points
+        self.__last_snm_point_update = last_snm_point_update
+        self.__bet_games = bet_games
+        self.__bet_wins = bet_wins
+        self.__bet_losses = bet_losses
+        self.__bet_game = bet_game
+        self.__ongoing_bets = ongoing_bets
 
     @property
     def userID(self):
@@ -96,19 +128,75 @@ class User:
         return self.__rps_game
 
     @property
-    def rps_kd(self):
+    def rps_kd(self):   # Returns the users rock paper scissors win/loss ratio
         return self.__rps_wins / (self.__rps_losses if self.__rps_losses != 0 else 1)
 
-    def play_rps(self):
+    @property
+    def snm_points(self):
+        self.update_snm_points()
+        return self.__snm_points
+
+    @property
+    def last_snm_point_update(self):
+        return self.__last_snm_point_update
+
+    @property
+    def bet_games(self):
+        return self.__bet_games
+
+    @property
+    def bet_wins(self):
+        return self.__bet_wins
+
+    @property
+    def bet_losses(self):
+        return self.__bet_losses
+
+    @property
+    def bet_game(self):
+        return self.__bet_game
+
+    @property
+    def ongoing_bets(self):
+        return self.__ongoing_bets
+
+    def update_snm_points(self):
+        date = datetime.datetime.now()
+        print(date, " ::: ",self.__last_snm_point_update)
+        self.__snm_points += int((date - self.__last_snm_point_update).total_seconds() / 60)
+        self.__last_snm_point_update = date
+
+    def play_rps(self):     # Creates a rock paper scissors game and stores it
         self.__rps_game = RPSGame(self.__userID)
 
-    def game_over_rps(self):
+    def game_over_rps(self):    # Extracts info from rock paper scissors game then deletes it
         if self.__rps_game.winner == self.__userID:
             self.__rps_wins += 1
         else:
             self.__rps_losses += 1
         self.__rps_games += 1
         self.__rps_game = None
+
+    def start_bet(self, opts):
+        self.__bet_game = Bet(self.__userID, opts)
+
+    def end_own_bet(self):
+        self.__bet_game = None
+
+    def end_bet(self, betID, winning_opt):
+        try:
+            if self.__ongoing_bets[betID][0] == winning_opt:
+                self.__bet_wins += 1
+            else:
+                self.__bet_losses += 1
+            self.__bet_games += 1
+            self.__ongoing_bets.pop(betID)
+        except KeyError: pass
+
+    def choose_bet(self, betID, opt, amount):
+        self.update_snm_points()
+        self.__ongoing_bets[betID] = [opt, amount]
+        self.__snm_points -= amount
 
     def __str__(self):
         return str(vars(self))
--- SNMBot.py
+++ SNMBot.py
@@ -1,17 +1,28 @@
+import asyncio
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio
 import time
-from DiscordBots.BotResources import *
+from random import choice
+from blib import *
+try: from BotResources import *
+except ModuleNotFoundError: from DiscordBots.BotResources import *
 
 
 Client = discord.Client()
 client = commands.Bot(command_prefix="")
 botName = "SNMBot#8413"
 
+print(eval("datetime.datetime(2018, 7, 15, 17, 25, 47, 427412)"))
+
 users = {}
-try:
+wordFilter = {
+    "communist": "capitalist",
+    "communism": "capitalism",
+    "capitalist": "communist",
+    "capitalism": "communism"
+}
+try:    # Tries to extract existing user data from storage and insert it into the 'users' dict
     with open("users.txt") as f:
         userdict = eval(f.read())
         for x in userdict.values():
@@ -26,7 +37,17 @@ try:
                     x["_User__rps_game"]["_RPSGame__winner"],
                     x["_User__rps_game"]["_RPSGame__comp_wins"],
                     x["_User__rps_game"]["_RPSGame__user_wins"]
-                ) if x["_User__rps_game"] is not None else None
+                ) if x["_User__rps_game"] is not None else None,
+                x["_User__snm_points"],
+                x["_User__last_snm_point_update"],
+                x["_User__bet_games"],
+                x["_User__bet_wins"],
+                x["_User__bet_losses"],
+                Bet(
+                    x["_User__userID"],
+                    x["_User__bet_game"]["_Bet__opts"]
+                ) if x["_User__bet_game"] is not None else None,
+                x["_User__ongoing_bets"]
             )
 except FileNotFoundError: print("file not found")
 
@@ -39,13 +60,16 @@ async def on_ready():
 @client.event
 async def on_message(mes):
     if str(mes.author) != botName: #str(mes.channel.server) == "Bot Test Realm": # for debuggin
-        try:
-            print(users[mes.author.id].userID + ": " + mes.content + "\nResponse:")
-        except KeyError:
-            users[mes.author.id] = User(mes.author.id)
-            print(mes.author.id + ": " + mes.content + "\nResponse:\nCreated new user")
+        try:    # Tries to print the users id, name and message contents in a manner where if the user has not been created by the bot it will cause an error and get caught by the except statement
+            print(users[mes.author.id].userID + "\n" + str(mes.author) + ": " + mes.content + "\nResponse:")
+        except KeyError:    # Creates a new user
+            users[mes.author.id] = User(mes.author.id, last_snm_point_update=datetime.now())
+            print(mes.author.id + "\n" + str(mes.author) + ": " + mes.content + "\nResponse:\nCreated new user")
         user = users[mes.author.id]
-        if mes.content.lower().startswith("!rpsrank"):
+        '''     Filters and other higher priority checks      '''
+
+        '''     Commands and other lower priority checks      '''
+        if mes.content.lower().startswith("!rpsrank"):  # Returns the players rank in rock paper scissors
             print("Showed user their rps rank")
             ranks = sorted(list(users.values()), key=lambda x: x.rps_kd, reverse=True)
             await client.send_message(
@@ -79,6 +103,51 @@ async def on_message(mes):
             else:
                 print("Informed user that they have not started a rps game")
                 await client.send_message(mes.channel, "You first have to start a game by typing *!playrps* <@" + mes.author.id + ">!")
+        elif mes.content.lower().startswith("!decide "):
+            print("Decided for user")
+            await client.send_message(
+                mes.channel,
+                "Hmmmm... Ok <@" + user.userID + ">, let's go for:\n" + choice(mes.content[len("!decide "):].split(","))
+            )
+        elif mes.content.lower().startswith("!snmp"):
+            print("Displayed users snmp")
+            await client.send_message(mes.author, "You have " + str(user.snm_points) + "SNM Points <@" + user.userID + ">!")
+        elif mes.content.lower().startswith("!startbet "):
+            if user.bet_game is not None:
+                print("Told use to end their ongiong bet first")
+                await client.send_message(mes.channel, "You first have to end your other bet <@" + user.userID + ">!\nTo end a bet just type '!endbet [winning_option]' (Example !endbet 4)\n" + "".join([str(x) + ": " + elem + "\n" for x, elem in enumerate(user.bet_game.opts)]))
+            else:
+                print("Started bet for user")
+                user.start_bet(mes.content[len("!startbet "):].split(","))
+                await client.send_message(mes.channel, "Alright <@" + user.userID + ">, I just started a bet. Everybody join in by betting your SNM Points!\nTo bet your points just type: '!bet @[user] [option] [amount]' (Example: !bet @SNMBot 2 69)\nTo find out how many SNM Points you have just type !snmp\nTo end a bet just type '!endbet [winning_option]' (Example !endbet 4)\nThe options are:\n" + "".join([str(x) + ": " + elem + "\n" for x, elem in enumerate(user.bet_game.opts)]))
+        elif mes.content.lower().startswith("!bet "):
+            try:
+                bet_user = mes.mentions[0].id
+                if user == bet_user:
+                    print("Alerted user that they can not bet on they're own bets")
+                    await client.send_message(mes.author, "You cannot bet on your own bets <@" + user.userID + ">!")
+                elif users[bet_user].bet_game is None:
+                    print("Alert user that the bet user has no ongoing bets")
+                    await client.send_message(mes.author, "<@" + bet_user + "> has no ongoing bets!")
+                else:
+                    if int(mes.content.split()[3]) <= user.snm_points:
+                        print("Betted for the user")
+                        user.choose_bet(bet_user, int(mes.content.split()[2]), int(mes.content.split()[3]))
+                        await client.send_message(mes.author, "Your bet went through and you now have " + str(user.snm_points) + "SNMP left until the bet ends!")
+                    else:
+                        print("Alerted user that they don't have enough snmp")
+                        await client.send_message(mes.author, "You can't afford to bet " + mes.content.split()[3] + " <@" + user.userID + ", you broke ass fuck!")
+            except KeyError:
+                print("Alerted user that bet user does not exit")
+                await client.send_message(mes.author, "User <@" + bet_user + "> does not exist!")
+        elif "no u" in mes.content.lower():
+            print("Corrected users pubescent insult")
+            await client.send_message(
+                mes.channel,
+                "No U has never been an original insult <@" + user.userID + ">, you're simply stealing it from Anti-Uranium political groups!\nsum(Silicon, Carbon, Potassium), sum(Baron, Uranium, Radon)\nsum(Germanium, Tantalum), sum(Rhenium, Potassium, Tellurium)"
+            )
+
+
         print()
         with open("users.txt", "w") as f:
             f.write(str(users))
